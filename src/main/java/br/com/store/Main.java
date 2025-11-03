6da1096e8f8e79d092ff3583c5aa5802459f0ba6
Allan Rodrigues <allanrodriguesdeveloper>
2025-11-02 22:25:30 -0300

    #5 Menu Created Client

--- src/main/java/br/com/store/Main.java
+++ src/main/java/br/com/store/Main.java
@@ -1,6 +1,7 @@
 package br.com.store;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import br.com.store.dao.ClienteDao;
 import br.com.store.model.Client;
@@ -8,16 +9,35 @@ import br.com.store.model.Client;
 public class Main {
     public static void main(String[] args) throws SQLException {
         ClienteDao clienteDAO = new ClienteDao();
+        Scanner sc = new Scanner(System.in);
 
-        var client = clienteDAO.list();
-        var clientID = clienteDAO.listId(4);
+        while (true) {
+            System.out.println("\n--- CRUD Clientes (Java Puro + JDBC) ---");
+            System.out.println("1. Criar Cliente");
+            System.out.println("2. Gerenciar Clientes (Listar/Atualizar/Deletar)");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
 
-        for (Client c : clientID) {
-            System.out.printf("ID: %d, Nome: %s, Email: %s\n", c.getId(), c.getName(), c.getEmail());
-
+            int option = sc.nextInt();
+            sc.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.print("Nome: ");
+                    String name = sc.nextLine();
+                    System.out.print("Email: ");
+                    String email = sc.nextLine();
+                    System.out.print("Telefone: ");
+                    String contact = sc.nextLine();
+                    Client client = new Client(name, email, contact);
+                    clienteDAO.save(client);
+                    System.out.printf("Parabens %s Cadastro realizado com sucesso", name);
+                    break;
+                case 2:
+                default:
+                    break;
+            }
         }
 
-    
     }
 
 }
